e326f4d2cff1dc7698066a0e6e4bf5779593fb8e
Unknown <b.sv@free.fr>
2018-04-03 15:55:48 +0200

    Add OnClickListener Event to a Cardview
    Pour tous les boutons actuellement, cela mènent vers une page WIP. Le temps de développer chaque fonction sur les boutons qu'on souhaite garder. Une feature doit être créée pour chaque page afin de garder quelque chose de propre sur le projet Git, merci.

--- app/src/main/java/beacon/projetco/dii/polytech/tours/univ/beaconsfinder/MainActivity.java
+++ app/src/main/java/beacon/projetco/dii/polytech/tours/univ/beaconsfinder/MainActivity.java
@@ -1,13 +1,44 @@
 package beacon.projetco.dii.polytech.tours.univ.beaconsfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private CardView adminCard, gameCard, languageCard, day_nightCard, infosCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Defining Cards
+        adminCard = findViewById(R.id.admin_card);
+        gameCard = findViewById(R.id.game_card);
+        languageCard = findViewById(R.id.language_card);
+        day_nightCard = findViewById(R.id.day_night_card);
+        infosCard = findViewById(R.id.infos_card);
+
+        // Add Click Listener to the cards
+        adminCard.setOnClickListener(this);
+        gameCard.setOnClickListener(this);
+        languageCard.setOnClickListener(this);
+        day_nightCard.setOnClickListener(this);
+        infosCard.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i ;
+
+        switch (view.getId()) {
+            case R.id.admin_card : i = new Intent(this, WIP.class); startActivity(i); break;
+            case R.id.game_card : i = new Intent(this, WIP.class); startActivity(i); break;
+            case R.id.language_card : i = new Intent(this, WIP.class); startActivity(i); break;
+            case R.id.day_night_card : i = new Intent(this, WIP.class); startActivity(i); break;
+            case R.id.infos_card : i = new Intent(this, WIP.class); startActivity(i); break;
+            default:break;
+        }
     }
 }
