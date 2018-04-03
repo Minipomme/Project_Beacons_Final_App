cb9041d9d6321e133f8eaba9e87b99ad0c7e016d
Brandon SIMON-VERMOT <b.sv@free.fr>
2018-04-03 23:30:20 +0200

    Activity Class fixed

--- app/src/main/java/beacon/projetco/dii/polytech/tours/univ/beaconsfinder/GameActivity.java
+++ app/src/main/java/beacon/projetco/dii/polytech/tours/univ/beaconsfinder/GameActivity.java
@@ -3,7 +3,7 @@ package beacon.projetco.dii.polytech.tours.univ.beaconsfinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Game extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
