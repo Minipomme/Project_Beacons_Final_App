df94f4a3e61cce70137e2906cadc23be1c6ddec3
Unknown <b.sv@free.fr>
2018-04-03 14:59:44 +0200

    Dashboard only
    Développement du menu principal, seulement l'interface est faite, l'action sur les boutons n'est pas encore implémentée cependant une animation est présente.

---
+++ app/src/main/java/beacon/projetco/dii/polytech/tours/univ/beaconsfinder/MainActivity.java
@@ -0,0 +1,13 @@
+package beacon.projetco.dii.polytech.tours.univ.beaconsfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
