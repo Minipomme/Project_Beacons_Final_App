74e4d8e4053f0d79c4049f152968f98d642bc367
Brandon SIMON-VERMOT <b.sv@free.fr>
2018-05-01 00:44:20 +0200

    Fin Implémentation splash screen

--- app/src/main/java/beacon/projetco/dii/polytech/tours/univ/beaconsfinder/SplashActivity.java
+++ app/src/main/java/beacon/projetco/dii/polytech/tours/univ/beaconsfinder/SplashActivity.java
@@ -1,8 +1,46 @@
 package beacon.projetco.dii.polytech.tours.univ.beaconsfinder;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by Minipomme on 30/04/2018.
  */
 
-public class SplashActivity {
+public class SplashActivity extends AppCompatActivity {
+    private TextView TVLoading;
+    private ImageView IVLogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        TVLoading = findViewById(R.id.TVloading);
+        IVLogo = findViewById(R.id.IVlogo);
+        Animation LoadAnim = AnimationUtils.loadAnimation(this, R.anim.splash_transition);
+
+        TVLoading.startAnimation(LoadAnim);
+        IVLogo.startAnimation(LoadAnim);
+
+        final Intent i = new Intent(this, MainActivity.class);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+
+        timer.start();
+    }
 }
